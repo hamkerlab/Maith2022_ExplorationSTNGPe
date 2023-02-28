2e9a103745cb5ee10141458dd8b3cdcdde628545
olmai <oli_maith@gmx.de>
2023-02-28 13:18:02 +0100

    iScience revision: added simulations 001f

---
+++ simulations/001f_rare_experiment/1_srcSim/run.sh
@@ -0,0 +1,24 @@
+startTime=$(date +%s)
+
+let parallel=5
+let durchgaenge=1
+
+for durchgang in $(seq $durchgaenge); do
+	startdurchgangTime=$(date +%s)
+        for i in $(seq $parallel); do
+                let y=$i+$parallel*$((durchgang - 1))
+                python parallel.py --simID $y &
+        done
+        wait
+        sleep 5
+	rm -r *annarchy*
+	endTime=$(date +%s)
+	dif=$((endTime - startdurchgangTime))
+    echo "${durchgang} ${dif}" >> fertig_zeit.txt
+	sleep 5
+done
+
+endTime=$(date +%s)
+dif=$((endTime - startTime))
+echo $dif >> fertig_zeit.txt
+
